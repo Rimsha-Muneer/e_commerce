c75e26077f8ea85b104e1ddadb61f6533ace47cf
Rimsha Muneer <rimshamuneer126@gmail.com>
2024-03-18 19:30:14 +0500

    theme

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,3 +1,7 @@
+ import 'package:e_commerce_app/utils/theme/custom_theme/appbar_theme.dart';
+import 'package:e_commerce_app/utils/theme/custom_theme/bottom_sheet.dart';
+import 'package:e_commerce_app/utils/theme/custom_theme/elevated_button_theme.dart';
+import 'package:e_commerce_app/utils/theme/custom_theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
@@ -8,7 +12,21 @@ class AppTheme {
     brightness: Brightness.light,
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: AppAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: AppBottomSheet.lightBottomSheetTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+     useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: AppTextTheme.darkTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.darkElevatedButtonTheme,
+    appBarTheme: AppAppBarTheme.darkAppBartheme,
+    bottomSheetTheme: AppBottomSheet.darkBottomSheetTheme,
 
   );
-  static ThemeData darkTheme = ThemeData();
 }
